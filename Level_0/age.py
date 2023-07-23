bbfe91208a7609a99d553f211e79454c1a497e4a
groom2hub <groomwnsgur35@gmail.com>
2023-07-23 23:14:27 +0900

    Project: angle

---
+++ Level_0/age.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+def solution(age):
+    answer = 2023 - age
+    return answer
---
+++ Level_0/division.py
@@ -0,0 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120806
+def solution(num1, num2):
+    answer = int(num1 / num2 * 1000)
+    return answer
