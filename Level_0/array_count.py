29d79499767a34b8dfff18d32ff945c5d63b42f6
groom2hub <groomwnsgur35@gmail.com>
2023-08-17 19:39:18 +0900

    Project: Count down

---
+++ Level_0/array_count.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+def solution(start, end):
+    answer = []
+    for i in range(start, end + 1):
+        answer.append(i)
+    return answer
