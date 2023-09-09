3ca10f042c565286a9da9009ce43292233cc32f2
groom2hub <groomwnsgur35@gmail.com>
2023-09-09 11:43:35 +0900

    Project: Count up

--- Level_0/count_up.py
+++ Level_0/count_up.py
@@ -1,5 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181920
 
 def solution(start_num, end_num):
-    answer = []
+    answer = [i for i in range(start_num, end_num-1)]
     return answer
