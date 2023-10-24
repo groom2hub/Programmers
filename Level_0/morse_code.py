cf46fdd6f83572c18cd4d220660271ca6b692064
groom2hub <groomwnsgur35@gmail.com>
2023-10-24 16:15:51 +0900

    Project: Morse code

---
+++ Level_0/morse_code.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120838
+
+def solution(letter):
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    morse_list = letter.split()
+    answer = ''.join(morse[i] for i in morse_list)
+    return answer
